b21e9fefc73bdc5cb96e9191810c3243beae1604
Michael Nutzenberger <michael.nutzenberger@smail.inf.h-brs.de>
2019-07-05 18:11:00 +0200

    site specific individual application settings. step-2

--- ccm.course_monitoring.js
+++ ccm.course_monitoring.js
@@ -94,7 +94,8 @@
                 !window.jQuery && await this.ccm.load( "https://code.jquery.com/jquery-3.4.0.min.js" );
 
                 let localStoreName = window.location.hostname + window.location.pathname;
-                console.log(self.utils.hash.md5(localStoreName));
+                self.local = await self.ccm.store({ "name": "cm-settings-" + self.utils.hash.md5(localStoreName) });
+
                 if ( window.SharedWorker ) {
                     for (let i = 0; i < 4; i++) {
                         try {
